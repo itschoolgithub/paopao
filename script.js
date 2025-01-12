d25b802b9a79f85160c8b546fa4e49aebb0b4030
itschoolgithub <itschoolgithub@mail.ru>
2025-01-12 09:58:18 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,64 +1,137 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const gameFieldHtml = document.querySelector(".game-field");
-    let gameField = [];
-    let pokemons = [];
-    let activeCell = null;
+    const gameField = document.querySelector(".game-field");
+    let cards = [];
+    let activeCard = null;
 
     generateGameField();
     drawGameField();
 
     function generateGameField() {
-        for (let i = 0; i < 9; i++) {
-            for (let j = 0; j < 16; j++) {
-                gameField.push({
-                    type: i,
+        let temp = [];
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 18; j++) {
+                temp.push({
+                    type: j,
                     order: Math.random()
                 });
             }
         }
 
-        gameField.sort(function (pokemon1, pokemon2) {
-            return pokemon1.order - pokemon2.order;
+        temp.sort(function (card1, card2) {
+            return card1.order - card2.order;
         });
 
-        for (let i = 0; i < 9; i++) {
-            pokemons.push([]);
-            for (let j = 0; j < 16; j++) {
-                pokemons[i].push(gameField[9 * i + j].type);
+        for (let y = 0; y < 9; y++) {
+            cards.push([]);
+            for (let x = 0; x < 16; x++) {
+                cards[y].push(temp[16 * y + x].type);
             }
         }
+        console.log(cards);
     }
 
     function drawGameField() {
         let html = '';
-        pokemons.forEach(function (row, i) {
-            row.forEach(function (pokemon, j) {
+        cards.forEach(function (row, y) {
+            row.forEach(function (card, x) {
                 html += `<img
                     class="game-cell"
-                    data-x="${i}" data-y="${j}"
-                    src="img/pokemon-${pokemon+1}.jpg"
+                    data-type="${card}" data-x="${x}" data-y="${y}" 
+                    src="img/card-${card+1}.jpg"
                 >`;
             });
         });
-        gameFieldHtml.innerHTML = html;
+        gameField.innerHTML = html;
     }
 
-    gameFieldHtml.addEventListener('click', function (event) {
-        if (event.target.classList.contains('game-cell')) {
-            if (activeCell) {
-                document.querySelector('.game-cell-active').classList.remove('game-cell-active');
-                activeCell = null;
-                check(event.target);
+    gameField.addEventListener('click', function (event) {
+        if (event.target.classList.contains('game-cell') && !event.target.classList.contains('game-cell-hide')) {
+            if (activeCard) {
+                activeCard.classList.remove('game-cell-active');
+                check(activeCard, event.target);
+                activeCard = null;
             } else {
-                event.target.classList.add('game-cell-active');
-                activeCell = {
-                    x: event.target.dataset.x,
-                    y: event.target.dataset.y,
-                };
+                activeCard = event.target;
+                activeCard.classList.add('game-cell-active');
             }
         }
     });
 
-    function check(target) {
+    function check(card1, card2) {
+        if (card1 == card2) {
+            return;
+        }
+
+        if (card1.dataset.type != card2.dataset.type) {
+            return;
+        }
+
+        let directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+
+        let queue = [
+            {
+                x: +card1.dataset.x,
+                y: +card1.dataset.y,
+                direction: null,
+                lines: 0,
+                path: []
+            }
+        ];
+
+        while (queue.length > 0) {
+            let current = queue.shift();
+            for (let i = 0; i < 4; i++) {
+                let temp = JSON.parse(JSON.stringify(current));
+                let x = temp.x + directions[i][0];
+                let y = temp.y + directions[i][1];
+                temp.lines = 
+                    (temp.direction !== null && temp.direction[0] == directions[i][0] &&
+                    temp.direction[1] == directions[i][1])
+                    ? temp.lines : temp.lines + 1;
+                if (
+                    (x >= -1 && x <= 16 && y >= -1 && y <= 9)
+                    && (
+                        typeof cards[y] == 'undefined'
+                        || (
+                            typeof cards[y] != 'undefined' && typeof cards[y][x] == 'undefined'
+                        )
+                        || cards[y][x] == null
+                    )
+                    && (
+                        (
+                            typeof temp.path[y] == 'undefined'
+                            || temp.path[y] == null
+                            || typeof temp.path[y][x] == 'undefined'
+                            || temp.path[y][x] == null
+                        )
+                        && temp.lines <= 3
+                    )
+                ) {
+                    if (typeof temp.path[y] == 'undefined' || temp.path[y] == null) {
+                        temp.path[y] = [];
+                    }
+                    temp.path[y][x] = true;
+                    queue.push({
+                        x: x,
+                        y: y,
+                        direction: directions[i],
+                        lines: temp.lines,
+                        path: temp.path
+                    });
+                } else if (x == card2.dataset.x && y == card2.dataset.y && temp.lines <= 3) {
+                    console.log('удаляем карточку', temp);
+                    removeCard(card1, card2);
+                    break;
+                }
+            }
+        }
+    }
+
+    function removeCard(card1, card2) {
+            cards[card1.dataset.y][card1.dataset.x] = null;
+            cards[card2.dataset.y][card2.dataset.x] = null;
+            card1.classList.add('game-cell-hide');
+            card2.classList.add('game-cell-hide');
     }
 });
